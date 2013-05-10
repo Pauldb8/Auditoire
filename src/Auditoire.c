a01655a1adbc61a625a1a70420edb98c354def4e
Paul De Buck <polichinel8@gmail.com>
2013-05-10 19:03:43 +0200

    Création via Eclipse du Projet Auditoire. V0.1
    Signed-off-by: Paul De Buck <polichinel8@gmail.com>

---
+++ src/Auditoire.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Auditoire.c
+ Author      : Paul, Célien And Steeves
+ Version     :
+ Copyright   : Do not reproduce without the authors' authorization.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
