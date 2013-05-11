e8d4998dacdd62a4a2d78fb9e0b44047e52a8de3
Paul De Buck <polichinel8@gmail.com>
2013-05-11 13:55:33 +0200

    Ajout d'une étape en plus au main, pour choisir la section à charger.
    Signed-off-by: Paul De Buck <polichinel8@gmail.com>

--- src/main.c
+++ src/main.c
@@ -12,9 +12,21 @@
 #include <stdlib.h>
 #include "structures.h"
 #include "operationsChoix.h"
+#include "tools.h"
+#define URL_SECTIONS "src/Sauvegardes" //Où chercher les dossiers de Sections
 
 int main(void) {
+	int choix;
 	setvbuf(stdout, NULL, _IONBF, 0); //Pour Eclipse
+	effacerEcran();//Efface l'écran et affiche le titre du programme
+	printf("Que voulez-vous faire ?\n");
+	printf("\t1.Charger une section\n");
+	printf("\t2.Créer une nouvelle section");
+	printf("\n\nVotre choix : ");
+	choix = getNumber(1,2);
+	if(choix == 1)
+		choisirSectionACharger(URL_SECTIONS);
+	//todo: add else
 	T_Annee * tabAnnee; // Ecole (tabAnnneeSection)
     afficherMenuGlobal(tabAnnee);
 	return EXIT_SUCCESS;
