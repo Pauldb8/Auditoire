e2765273287b6d2f309b6777316fddf0906bc0cf
Paul De Buck <polichinel8@gmail.com>
2013-05-11 16:25:30 +0200

    Ajout du fichier defines.h
    Signed-off-by: Paul De Buck <polichinel8@gmail.com>

---
+++ src/defines.h
@@ -0,0 +1,25 @@
+/*
+ * defines.h
+ *
+ *  Created on: 11 mai 2013
+ *      Author: Pauldb
+ *         But: Ce fichier permet de centraliser les différents #defines.
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define MAX_CHAR 51
+#define URL_SECTIONS "src/Sauvegardes" //Où chercher les dossiers de Sections
+#define INCREMENTALLOC 3
+
+#define DEBUG 1
+#define TAILLE_MAX MAX_CHAR
+#define INCREMENTALLOC 3
+#define URI_FICHIERS "src/Sauvegardes/"
+#define FICHIER_TI "fichierParametrage"
+#define EXT ".txt"
+#define FICHIER_PARAMETRAGE URI_FICHIERS FICHIER_TI EXT
+
+
+#endif /* DEFINES_H_ */
