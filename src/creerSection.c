bced65a011f5322a5ffb9e3ce3335b078fec6d28
Mornor <cesliens@gmail.com>
2013-05-19 09:26:59 +0200

    Update creerSection.c
    Création d'une section fonctionelle ;)
    (choix 2 dans le tout premier Menu)

--- src/creerSection.c
+++ src/creerSection.c
@@ -7,12 +7,16 @@
 
 #include "structures.h"
 #include "tools.h"
+#include "sauvegardeFichier.h"
+#include "defines.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 T_Section creerSection(char * URL)
 {
     T_Section aRenvoyer;
+    int i, j, k;
+    char * url;
 
     effacerEcran();
     printf("*** Creation d'une nouvelle section *** \n\n");
@@ -20,15 +24,56 @@ T_Section creerSection(char * URL)
     printf("Nom de la section : ");
     fflush(stdin);
     gets(aRenvoyer.nom);
+    printf("\n");
     printf("Nombre d'annee de la section : ");
+    fflush(stdin);
     scanf("%d", &aRenvoyer.nbrAnnees);
 
-    // Création d'uine classe vide.
-    T_Annee *tabAnnees = NULL;
-    aRenvoyer.tabAnnees = tabAnnees;
+    aRenvoyer.tabAnnees = malloc(aRenvoyer.nbrAnnees * sizeof(T_Annee));
 
-    printf("TODO");
-    system("PAUSE");
+    for(i = 0 ; i < aRenvoyer.nbrAnnees ; i++)
+    {
+        printf("\n");
+        printf("\t.Nom de l'annee %d (ex : 1TI) : ", i+1);
+        fflush(stdin);
+        gets(aRenvoyer.tabAnnees[i].nomAnneeSection);
+        printf("\t\tNombre de classes de cette annees (%s) : ", aRenvoyer.tabAnnees[i].nomAnneeSection);
+        fflush(stdin);
+        scanf("%d", &aRenvoyer.tabAnnees[i].nbClasses);
+
+        aRenvoyer.tabAnnees[i].tabClasse = malloc(aRenvoyer.tabAnnees[i].nbClasses * sizeof(T_Classe));
+        aRenvoyer.tabAnnees[i].nomClasse = (char **) malloc(aRenvoyer.tabAnnees[i].nbClasses * sizeof(char*));
+
+        for(j = 0 ; j < aRenvoyer.tabAnnees[i].nbClasses ; j++)
+        {
+            aRenvoyer.tabAnnees[i].nomClasse[j] = (char *)malloc(MAX_CHAR * sizeof(char));
+            printf("\t\t\t-Nom de la classe %d : ", i+1);
+            fflush(stdin);
+            gets(aRenvoyer.tabAnnees[i].nomClasse[j]);
+        }
+
+        printf("\n");
+        printf("\t\tNombre de cours par etudiant : ");
+        scanf("%d", &aRenvoyer.tabAnnees[i].nbCoursParEtudiant);
+
+        aRenvoyer.tabAnnees[i].tabCours = malloc(aRenvoyer.tabAnnees[i].nbCoursParEtudiant * sizeof(T_Cours));
+
+        for(k = 0 ; k < aRenvoyer.tabAnnees[i].nbCoursParEtudiant ; k++)
+        {
+            printf("\t\t\t Nom du cours %d : ", i+1);
+            fflush(stdin);
+            gets(aRenvoyer.tabAnnees[i].tabCours[k].nomCours);
+            printf("\t\t\t Ponderation de ce cours : ");
+            fflush(stdin);
+            scanf("%d", &aRenvoyer.tabAnnees[i].tabCours[k].ponderation);
+        }
+
+
+    }
+
+    sauverFichierParametrage(aRenvoyer);
+    printf("La section est correctement creer, et correctement enregistree ! \n\n");
+    system("Pause");
     return aRenvoyer;
 }
 
